22e4ad2af4889f647a459818f1ac8c1e217f2f31
juxsalley <juxsalley@gmail.com>
2021-05-11 08:18:50 +0000

    add operate moudule for maths opeations using big.js

--- src/components/logic/operate.js
+++ src/components/logic/operate.js
@@ -1,13 +1,25 @@
+import Big from 'big.js';
+
 const operation = (total, next, buttonName) => {
+  const newTotal = Big(total);
+  const newNext = Big(next);
   let res = 0;
   switch (buttonName) {
     case '-':
-      res = total - next;
+      res = newTotal.minus(newNext);
       return res;
     case '+':
-      res = total + next;
+      res = newTotal.plus(next);
       return res;
-
+    case 'x' || 'X':
+      res = newTotal.mul(newNext);
+      return res;
+    case '%':
+      res = newTotal.mod(newNext);
+      return res;
+    case '/' || '÷':
+      res = newTotal.div(newNext);
+      return res;
     default:
       return false;
   }
