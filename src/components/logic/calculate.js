702e18250098e188b4480d4170b6fe9025273a0d
juxsalley <juxsalley@gmail.com>
2021-05-11 18:15:13 +0000

    implement changes require by TSE

--- src/components/logic/calculate.js
+++ src/components/logic/calculate.js
@@ -90,35 +90,3 @@ const calculate = (calculateData, buttonName) => {
 };
 
 export default calculate;
-
-// console.log(total);
-// if (isValidNumber(buttonName) && operation == null) {
-//   return { next: next + buttonName };
-// }
-// if (isValidNumber(next) && operation && !total) {
-//   return { next: `${next} ${operation}` };
-// }
-// if (isValidNumber(next) && operation && total && buttonName === '=') {
-//   return operate(total, next, buttonName);
-// }
-
-// if (buttonName === 'AC') {
-//   return { total: null, next: null, operation: null };
-// }
-// if (buttonName === '.') {
-//   return !next ? { next: `0${buttonName}` } : { next: `${next}${buttonName}` };
-// }
-// if (buttonName === '+/-') {
-//   return !total ? { next: operate(next, -1, '*') } :
-// { total: operate(total, -1, '*'), next: operate(next, -1, '*') };
-// }
-// if (buttonName === '%') {
-//   if (!next && !total) {
-//     return { total: null, next: null, operation: null };
-//   } if (next && !total) {
-//     return { next: operate(next, 100, '/') };
-//   } if (next && total) {
-//     const res = operate(next, 100, '/');
-//     return { total: operate(res, next, '*') };
-//   }
-// }
