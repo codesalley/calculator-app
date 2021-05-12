fed83d0d1c12cc852f1b88c5ae0ff0242e8eeec2
juxsalley <juxsalley@gmail.com>
2021-05-12 19:07:49 +0000

    implement negative test case

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,6 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import HomePage from '../components/Home';
 
+test('renders Homepage not 404 page', () => {
+  render(<HomePage />);
+  const res = screen;
+  expect(res).not.toBeNull();
+});
+
 test('renders Homepage properly and show header image', () => {
   render(<HomePage />);
   const res = screen.getAllByAltText(/intro-logo/i);
