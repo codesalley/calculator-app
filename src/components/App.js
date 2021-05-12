6768b2d57a216f2489fcc94e5f2146821117ec9f
juxsalley <juxsalley@gmail.com>
2021-05-12 11:33:30 +0000

    style calculator page

--- src/components/App.js
+++ src/components/App.js
@@ -32,6 +32,16 @@ function App() {
           </Route>
           <Route path="/calculator">
             <div className="container">
+              <div className="calculator-intro">
+                <h2>Let&lsquo;s do some Maths</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                  fugiat nulla pariatur.
+                </p>
+              </div>
               <div className="main-intro">
                 <div className="results-card">
                   <Display total={total} next={next} />
@@ -40,6 +50,7 @@ function App() {
                   <ButtonPanel onclick={submitTo} />
                 </div>
               </div>
+
             </div>
           </Route>
         </div>
